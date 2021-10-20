1c5d2d0af77d25c81477673f83cfa1e2bc2e57b7
Johann B <s11830056@students.duvalschools.org>
2021-10-20 09:00:51 -0400

    [5b] v0.5
    Imported scanner and added scanners for Username monster name and type of attack

---
+++ DamageDealt.java
@@ -0,0 +1,26 @@
+// <Johann Burke>, AP CSA Unit 1 Challenge, Andrew Jackson High School, <10/20/2021>, <8:36>, v0.5
+
+import java.util.Scanner;
+
+public class DamageDealt {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+
+        System.out.println("Please Input your UserName: ");
+
+        String userName = myScanner.nextLine();
+        String monsterName = myScanner.nextLine();
+        String weaponAttack = myScanner.nextLine();
+        
+        System.out.println("your username is: " + userName);
+
+        System.out.println("please Imput name of attack: ");
+
+
+        System.out.println("your Attack is: " + weaponAttack);
+
+        System.out.println("Please input name of monster: ")
+
+
+    }
+}
